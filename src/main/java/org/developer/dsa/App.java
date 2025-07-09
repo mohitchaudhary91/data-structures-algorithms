6ea54545be58b3641c724dcf4b22b40d841391a6
Mohit Chaudhary <mohitch.work@gmail.com>
2025-07-09 19:36:02 +0530

    app-file-added
    app-file-added

---
+++ src/main/java/org/developer/dsa/App.java
@@ -0,0 +1,8 @@
+package org.developer.dsa;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a data structure algorithms repository");
+    }
+}
